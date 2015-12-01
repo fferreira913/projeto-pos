9ec9a759eb2b806aac0b02d517c77f830d5ae519
Fatinha de Sousa <fatinha.sg@hotmail.com>
2015-12-01 08:32:07 -0300

    Dao Comanda

---
+++ POS - PROJETO/business-app/src/main/java/com/business/negocio/DaoComanda.java
@@ -0,0 +1,36 @@
+package com.business.negocio;
+
+import com.business.core.app.Comanda;
+import javax.persistence.EntityManager;
+
+/**
+ *
+ * @author Fatinha de Sousa
+ */
+public class DaoComanda {
+
+    EntityManager em = Conection.getEntityManager();
+    
+    public boolean salvarComanda(Comanda comanda){
+        try {
+            em.getTransaction().begin();
+            em.persist(comanda);
+            em.getTransaction().commit();
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            em.getTransaction().getRollbackOnly();
+            return false;
+        }
+    }
+    
+    public boolean atualizarComanda(Comanda comanda){
+        try {
+            em.merge(comanda);
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+}
